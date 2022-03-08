c91cb5cbf72db9107777e2837052eefa26f21b9d
JagTheFriend <jagthefriend12@gmail.com>
2022-03-08 21:28:19 +0200

    Created a new function to remove the item from the todo list

--- src/services/core.service.ts
+++ src/services/core.service.ts
@@ -1,9 +1,18 @@
 import { User } from '@interfaces/users.interface';
-import { CreateNewTodo } from '@dtos/features.dto';
+import { CreateNewTodo, DeleteTodo } from '@dtos/features.dto';
 
 class CoreService {
   public async addTodo(user: User, data: CreateNewTodo): Promise<void> {
     user.todoList.push({ content: data.content, dateCreated: data.dateCreated });
+    user.save();
+  }
+
+  public async removeTodo(user: User, dateCreated: DeleteTodo): Promise<void> {
+    const arr = [];
+    for (const i of user.todoList) {
+      if (i.dateCreated !== dateCreated.dateCreated) arr.push(i);
+    }
+    user.todoList = arr;
     user.save();
   }
 }
