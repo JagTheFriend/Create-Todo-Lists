bb40b9f9ada8639f8b85ff62393c58368f1548ca
JagTheFriend <jagthefriend12@gmail.com>
2022-03-08 14:56:43 +0200

    Created the addTodo function

--- src/services/core.service.ts
+++ src/services/core.service.ts
@@ -2,10 +2,9 @@ import { User } from '@interfaces/users.interface';
 import { CreateNewTodo } from '@dtos/features.dto';
 
 class CoreService {
-  // public users = userModel;
-  public async addTodo(user: User, data: CreateNewTodo) {
-    console.log(data);
-    console.log(user);
+  public async addTodo(user: User, data: CreateNewTodo): Promise<void> {
+    user.todoList.push({ content: data.content, dateCreated: data.dateCreated });
+    user.save();
   }
 }
 
