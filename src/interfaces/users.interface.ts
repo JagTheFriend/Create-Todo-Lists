eb8ac3244984d0c17d1c969b855dc2bfa873e277
JagTheFriend <jagthefriend12@gmail.com>
2022-03-08 13:50:59 +0200

    Updated interface

--- src/interfaces/users.interface.ts
+++ src/interfaces/users.interface.ts
@@ -1,5 +1,11 @@
+export interface TodoList {
+  content: string;
+  dateCreated: string;
+}
+
 export interface User {
   _id: string;
   email: string;
   password: string;
+  todoList: TodoList[];
 }
