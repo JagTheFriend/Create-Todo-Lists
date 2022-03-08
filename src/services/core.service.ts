1d30de67a5eaf7fca2174bae3778619558288059
JagTheFriend <jagthefriend12@gmail.com>
2022-03-08 22:09:19 +0200

    Working on the service file

--- src/services/core.service.ts
+++ src/services/core.service.ts
@@ -1,19 +1,19 @@
 import { User } from '@interfaces/users.interface';
 import { CreateNewTodo, DeleteTodo } from '@dtos/features.dto';
+import { HttpException } from '@exceptions/HttpException';
+import { v4 as UUIDv4 } from 'uuid';
 
 class CoreService {
   public async addTodo(user: User, data: CreateNewTodo): Promise<void> {
     user.todoList.push({ content: data.content, dateCreated: data.dateCreated });
+    user.uniqueIds.push(UUIDv4());
     user.save();
   }
 
-  public async removeTodo(user: User, dateCreated: DeleteTodo): Promise<void> {
-    const arr = [];
-    for (const i of user.todoList) {
-      if (i.dateCreated !== dateCreated.dateCreated) arr.push(i);
-    }
-    user.todoList = arr;
-    user.save();
+  public async removeTodo(user: User, uniqueId: DeleteTodo): Promise<void> {
+    const index = user.uniqueIds.indexOf(uniqueId.uniqueId);
+    if (index === -1) throw new HttpException(400, `Invalid ID: ${uniqueId.uniqueId}`);
+    // user.todoList.
   }
 }
 
