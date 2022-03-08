84566cc40b987709d563bc4b18be03e88cef62d5
JagTheFriend <jagthefriend12@gmail.com>
2022-03-08 13:11:06 +0200

    Updated file

--- src/services/core.service.ts
+++ src/services/core.service.ts
@@ -1,5 +1,12 @@
+import { User } from '@interfaces/users.interface';
+import { CreateNewTodo } from '@dtos/features.dto';
+
 class CoreService {
   // public users = userModel;
+  public async addTodo(user: User, data: CreateNewTodo) {
+    console.log(data);
+    console.log(user);
+  }
 }
 
 export default CoreService;
