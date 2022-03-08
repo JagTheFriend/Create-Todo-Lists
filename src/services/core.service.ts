c0a0e68a9a2c6d27842fa6cd406f5e8b785478d3
JagTheFriend <jagthefriend12@gmail.com>
2022-03-08 22:17:29 +0200

    Created a way to remove items from todoList

--- src/services/core.service.ts
+++ src/services/core.service.ts
@@ -12,8 +12,10 @@ class CoreService {
 
   public async removeTodo(user: User, uniqueId: DeleteTodo): Promise<void> {
     const index = user.uniqueIds.indexOf(uniqueId.uniqueId);
-    if (index === -1) throw new HttpException(400, `Invalid ID: ${uniqueId.uniqueId}`);
-    // user.todoList.
+    if (index === -1) throw new HttpException(402, `Invalid ID: ${uniqueId.uniqueId}`);
+    user.uniqueIds.splice(index, index - 1);
+    user.todoList.splice(index, index - 1);
+    user.save();
   }
 }
 
