57da33a2a28cbb15be43e30d3c068205b43e6c0d
JagTheFriend <jagthefriend12@gmail.com>
2022-03-11 09:11:27 +0200

    Created a new route

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -3,7 +3,7 @@ import IndexController from '@controllers/index.controller';
 import { Routes } from '@interfaces/routes.interface';
 import authMiddleware from '@middlewares/auth.middleware';
 import validationMiddleware from '@middlewares/validation.middleware';
-import { CreateNewTodo, DeleteTodo } from '@dtos/features.dto';
+import { CreateNewTodo, DeleteTodo, UpdatedTodo } from '@dtos/features.dto';
 
 class IndexRoute implements Routes {
   public path = '/';
@@ -18,6 +18,7 @@ class IndexRoute implements Routes {
     this.router.get(`${this.path}dashboard`, authMiddleware, this.indexController.index);
     this.router.get(`${this.path}view`, authMiddleware, this.indexController.viewTodo);
     this.router.post(`${this.path}new`, authMiddleware, validationMiddleware(CreateNewTodo, 'body'), this.indexController.addTodo);
+    this.router.post(`${this.path}edit`, authMiddleware, validationMiddleware(UpdatedTodo, 'body'), this.indexController.editTodo);
     this.router.post(`${this.path}delete`, authMiddleware, validationMiddleware(DeleteTodo, 'body'), this.indexController.removeTodo);
   }
 }
